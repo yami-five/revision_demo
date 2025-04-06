3e3f65a3cddb302b22ad8be660676fb582302dd5
yami-five <konrad.rykala.94@gmail.com>
2025-04-06 16:58:19 +0200

    added saving, adding, removing

--- curve_editor.py
+++ curve_editor.py
@@ -1,4 +1,11 @@
 import pygame
+from enum import Enum
+
+class modes(Enum):
+    MODIFY = 1
+    ADD = 2
+    CHOOSE = 3
+    REMOVE = 4
 
 class point:
     def __init__(self, x, y,size,outline):
@@ -67,6 +74,11 @@ class curve:
             y+=3*self.points[2].get_y()*t*t*(1-t)
             y+=self.points[3].get_y()*t*t*t
             screen.set_at((int(x),int(y)),white)
+            
+    def get_point_by_pos(self,pos):
+        for p in self.points:
+            if p.is_clicked(pos):
+                return p
 
 class workspacePoint:
     def __init__(self, x, y):
@@ -122,21 +134,33 @@ class workspace:
         for i in range(0,width//8):
             pygame.draw.line(screen, midnight_blue, (x+i*8*self.get_scale(),y),(x+i*8*self.get_scale(),y+height*self.scale-1),1)
         for i in range(0,self.height//8):
-            pygame.draw.line(screen, midnight_blue, (x,y+i*8*self.get_scale()),(x+width*self.scale-1,y+i*8*self.get_scale()),1)
-        
+            pygame.draw.line(screen, midnight_blue, (x,y+i*8*self.get_scale()),(x+width*self.scale-1,y+i*8*self.get_scale()),1)  
     
     def out_of_workspace(self,pos):
         return (pos[0]<self.a.get_x() and pos[1]<self.a.get_y()) or (pos[0]>=self.b.get_x() and pos[1]>=self.b.get_y())
 
+def save_curves_to_file(curves,targetScreen):
+    result=""
+    with open('curve.txt', 'w') as file:
+        for c in curves:
+            for p in c.get_points():
+                x=int((p.get_x()-targetScreen.get_a().get_x())//targetScreen.get_scale())
+                y=int((p.get_y()-targetScreen.get_a().get_y())//targetScreen.get_scale())
+                result+=f'{x},{y},'
+        file.write(result[:-1])
+        
+
 pygame.init()
 
+delay=100
+currentMode=modes.MODIFY
+activeCurve=-1
+lastActiveCurve=-1
 width, height = 800, 600
 scale=3
 screen=pygame.display.set_mode((width,height))
 targetScreen=workspace(128,128,scale,screen)
 pygame.display.set_caption("curve editor")
-
-points=curve()
 
 white = (255,255,255)
 black = (0,0,0)
@@ -147,10 +171,14 @@ blue = (0,0,255)
 teal = (0,128,128)
 midnight_blue = (25,25,112)
 
+font = pygame.font.SysFont(None, 24)
+text_surface = font.render("ctrl + S - save to file    lmb + a - choose active curve    lmb + c - add curve    lmb + r - remove curve", True, green)
+
+curves=[]
 
 running=True
 clicked=False
-activePoint=0
+
 while running:
     screen.fill(black)    
     mousePos=pygame.mouse.get_pos()
@@ -158,19 +186,69 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-    if pygame.mouse.get_pressed()[0] and clicked is False:
-        if points.out_of_screen() and targetScreen.out_of_workspace(mousePos) is False:
-            points.add_curve_to_screen(mousePos)
+        if event.type==pygame.KEYDOWN:
+            keys = pygame.key.get_mods()
+            if keys & pygame.KMOD_CTRL:
+                if event.key == pygame.K_s:
+                    print("save")
+                    save_curves_to_file(curves, targetScreen)
+            if  event.key == pygame.K_a:
+                print("choose")
+                currentMode=modes.CHOOSE    
+            if  event.key == pygame.K_c:
+                print("add")
+                currentMode=modes.ADD
+            if  event.key == pygame.K_r:
+                print("remove")
+                currentMode=modes.REMOVE
+        if event.type == pygame.KEYUP:
+            currentMode=modes.MODIFY
+        
+    if pygame.mouse.get_pressed()[0] and clicked is False and currentMode==modes.ADD and delay==100:
+        curves.append(curve())
+        curves[len(curves)-1].add_curve_to_screen(mousePos)
+        delay=0
+        
+    if pygame.mouse.get_pressed()[0] and clicked is False and currentMode==modes.CHOOSE:
+        for i in range(0,len(curves)):
+            p=curves[i].get_point_by_pos(mousePos)
+            if p is not None:
+                lastActiveCurve=activeCurve
+                activeCurve=i
+                break
             
-    if points.out_of_screen() is False:
-        points.draw_lines(screen)
-        points.draw_curve(screen)
-        for p in points.get_points():
-            if p.isActive:
-                pygame.draw.rect(screen,green,(p.x-3,p.y-3,p.outline,p.outline))
+    if pygame.mouse.get_pressed()[0] and clicked is False and currentMode==modes.REMOVE:
+        curveToRemove=-1
+        for i in range(0,len(curves)):
+            p=curves[i].get_point_by_pos(mousePos)
+            if p is not None:
+                curveToRemove=i
+                break
+        if curveToRemove>=0:
+            del curves[curveToRemove]
+            if activeCurve==curveToRemove:
+                activeCurve=-1
+                lastActiveCurve=-1
+            if activeCurve>curveToRemove:
+                activeCurve-=1
+            if lastActiveCurve>curveToRemove:
+                lastActiveCurve-=1
+                      
+    for i in range(0,len(curves)):
+        curves[i].draw_lines(screen)
+        curves[i].draw_curve(screen)
+        for p in curves[i].get_points():
+            if i==activeCurve:
+                if p.isActive:
+                    pygame.draw.rect(screen,green,(p.x-3,p.y-3,p.outline,p.outline))
+                else:
+                    pygame.draw.rect(screen,blue,(p.x-3,p.y-3,p.outline,p.outline))
             else:
-                pygame.draw.rect(screen,blue,(p.x-3,p.y-3,p.outline,p.outline))
+                pygame.draw.rect(screen,red,(p.x-3,p.y-3,p.outline,p.outline))
             pygame.draw.rect(screen,white,(p.x,p.y,p.size,p.size))
+    
+    if(activeCurve>=0):
+        for p in curves[activeCurve].get_points():
             if (p.is_clicked(mousePos) or p.isActive) and targetScreen.out_of_workspace(mousePos) is False:
                 if pygame.mouse.get_pressed()[0] and clicked is False:
                     p.isActive=True
@@ -184,6 +262,10 @@ while running:
                 if p.isActive is False:
                     p.move_point(((p.get_x()//targetScreen.get_scale())*targetScreen.get_scale(),(p.get_y()//targetScreen.get_scale())*targetScreen.get_scale()))
 
+    screen.blit(text_surface, (20, height-100))
+    if delay<100:
+        delay+=1
+    
     pygame.display.flip()
 
 pygame.quit()
